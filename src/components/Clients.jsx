34f29aabe91b7503b3daac397f1ba46e632cbd5c
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-07-07 12:12:02 +0300

    Added Testimonials and Clients Layout

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import { clients } from "../constants"
+import styles
+  from "../style"
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <section className={`${styles.flexCenter}`}>
+      <div className={`${styles.flexCenter} flex-wrap`}>
+        {clients.map((client) => (
+          <div key={client.id} className={`flex-1 ${styles.flexCenter}`}>
+            <img src={client.logo} alt="client" className="w-[50%] h-[50%] object-contain" />
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
