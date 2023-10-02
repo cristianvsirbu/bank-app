4bc996ac1740eda0f385c32e7be59dd712dd56b9
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-10-02 19:13:45 +0300

    Added Project Mars integration

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react"
 import { clients } from "../constants"
-import styles
-  from "../style"
+import styles from "../style"
 import CardAnimation from "./CardAnimation"
 const Clients = () => {
+  const [imageSource, setImageSource] = useState('');
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newImageSource = window.innerWidth > 767 ? '/src/assets/sponsor.webp' : '/src/assets/sponsor_mobile.svg';
+      setImageSource(newImageSource);
+    };
+
+    handleResize(); 
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <section id="clients" className={`${styles.flexCenter} flex flex-col`}>
       <CardAnimation interval={300} animationClassName='stats'>
@@ -15,6 +30,16 @@ const Clients = () => {
               <img src={client.logo} alt="client" className="w-[70%] h-[70%] object-contain mb-8 box-shadow-20" />
             </div>
           ))}
+          <div className="mt-10">
+            <h2 className={`${styles.heading2} text-center mt-10`}>We are now sponsoring:</h2>
+            <a
+              href="https://project-mars-serverless.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={imageSource} alt="screenshot1" className="w-full" />
+            </a>
+          </div>
         </CardAnimation>
       </div>
     </section>
