8cc02120102405353f1fd36497e9aa2bc5c746ce
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-04-28 15:32:00 +0300

    typo fix

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 const TestimonialCard = forwardRef(({ content, name, title, img }, ref) => {
   return (
     <CardAnimation interval={500} animationClassName="stats">
-      <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card ">
+      <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
         <CardAnimation interval={500} animationClassName="stats">
           <img src={quotations} alt="quotations" className="w-[5rem] h-[5rem] object-contain" />
           <p className="font-neue font-normal text-[18px] leading-[30.8px] text-white my-10">
@@ -19,7 +19,7 @@ const TestimonialCard = forwardRef(({ content, name, title, img }, ref) => {
             <img src={img} alt={name} className="w-[72px] h-[72px] rounded-full" />
             <div className="flex flex-col ml-4">
               <CardAnimation interval={500} animationClassName="stats">
-                <h4 className="font-neue font semibold text-[20px] leading-[32px] text-white">
+                <h4 className="font-neue font-semibold text-[20px] leading-[32px] text-white">
                   {name}
                 </h4>
                 <p className="font-neue font-normal text-[16px] leading-[24px] text-dimWhite">
